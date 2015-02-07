c420cbd90180e08cab35542d3adb5232a7233150
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-02-07 16:31:48 -0600

    create nib, creat subview animation

---
+++ Ping2/Ping2/Classes/NewEventView.h
@@ -0,0 +1,13 @@
+//
+//  NewEventView.h
+//  Ping2
+//
+//  Created by Andy Bayer on 2/7/15.
+//  Copyright (c) 2015 Andy Bayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewEventView : UIView
+
+@end
---
+++ Ping2/Ping2/Classes/NewEventView.m
@@ -0,0 +1,21 @@
+//
+//  NewEventView.m
+//  Ping2
+//
+//  Created by Andy Bayer on 2/7/15.
+//  Copyright (c) 2015 Andy Bayer. All rights reserved.
+//
+
+#import "NewEventView.h"
+
+@implementation NewEventView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
